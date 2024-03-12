101fb152c6a3b1c25f471149f9f5d6cbb45f8ef0
ahamed irfan <ahamedirfani987@gmail.com>
2024-03-12 15:17:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './Components/Dashboard';
+import Product from './Components/Product';
+import Order from './Components/Order';
+import './App.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+
+        <nav className="navbar">
+          <ul className="nav-list">
+          <li className="nav-item">
+              <Link to="https://careers.entnt.in/role/a4821cd6-cd96-41c4-b650-bd1a30191ec8" className="nav-link">Entnt Front End Assignment</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/product" className="nav-link">Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/order" className="nav-link">Orders</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/order" element={<Order />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
